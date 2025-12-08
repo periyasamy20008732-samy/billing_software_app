ea6e9eea6a1dc87771f1d81761a0d82884abdc34
pssoftwaredevops8732-debug <pssoftwaredevops8732@gmail.com>
2025-12-08 11:20:40 +0530

    add asset

--- resources/views/welcome.blade.php
+++
@@ -1,7 +0,0 @@
-<hr>priyadharshini</hr>
-<h6>priyadharshini</h6>
-<h5>priyadharshini</h5>
-<h4>priyadharshini</h4>
-<h3>priyadharshini</h3>
-<h2>priyadharshini</h2>
-<h1>priyadharshini</h1>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/newroute', function () {
     return view('index');
+});
+Route::get('/pricing', function () {
+    return view('pricing');
+});
+
+Route::get('/about-us', function () {
+    return view('about');
+});
+Route::get('/partner-program', function () {
+    return view('partnerprogram');
+});
+
+Route::get('/contact-us', function () {
+    return view('contact');
 });
